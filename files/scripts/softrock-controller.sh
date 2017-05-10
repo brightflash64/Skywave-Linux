767550f778ac9bdce02fa598282891db50980e1c
brightflash64 <wm_munny92@yahoo.com>
2017-05-10 23:46:34 +0000

    Update softrock-controller.sh

--- files/scripts/softrock-controller.sh
+++ files/scripts/softrock-controller.sh
@@ -2,16 +2,15 @@
 
 no() {
 gnome-terminal -e 'softrock --iq --samplerate 48000' &
-sleep 2
-gnome-terminal -e 'dspserver --lo 9000' &
-sleep 2
-QtRadio
-killall softrock
-killall dspserver
+start
 }
 
 yes() {
 gnome-terminal -e 'softrock --iq --si570 --samplerate 48000' &
+start
+}
+
+start() {
 sleep 2
 gnome-terminal -e 'dspserver --lo 9000' &
 sleep 2
@@ -20,35 +19,22 @@ killall softrock
 killall dspserver
 }
 
-echo ""
-echo "Enable networked audio with the PulseAudio Preferences Application."
-echo ""
-read -p "Create keys for this session? (Y,n) " choice1
+echo '\nEnable networked audio with the PulseAudio Preferences Application.\n'
+read -p 'Create keys for this session? (Y,n) ' choice1
 case "$choice1" in 
   y|Y ) 
   openssl genrsa -out pkey 2048
   openssl req -new -key pkey -out cert.req
   openssl x509 -req -days 365 -in cert.req -signkey pkey -out cert
-  echo ""
-  echo "Okay, one more question..."
-  echo ""
-;;
-  n|N )
-  echo ""
-  echo "Okay, one more question..."
-  echo ""
 ;;
 esac
 
-read -p "Is your Softrock using an Si570 Tuner? (y/n)? " choice2
+echo '\n\nUse the SubRX to tune within the passband.\n\n'
+read -p 'Is your Softrock using an Si570 Tuner? (y/n)? ' choice2
 case "$choice2" in 
   y|Y ) yes;;
   n|N ) no;;
-  * )
-  echo ""
-  echo "Please reply yes or no."
-  echo ""
+  * ) read -p 'Please reply yes or no. ' X
 ;;
 esac
-
 
