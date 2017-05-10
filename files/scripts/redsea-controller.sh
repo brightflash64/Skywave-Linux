7062e984188ac8f6655343415a12792e40fe0394
brightflash64 <wm_munny92@yahoo.com>
2017-05-10 23:43:54 +0000

    Update redsea-controller.sh

--- files/scripts/redsea-controller.sh
+++ files/scripts/redsea-controller.sh
@@ -2,13 +2,11 @@
 Encoding=UTF-8
 
 start() {
-cd /usr/local/bin/
-perl ./redsea.pl $freq
+M=M
+rtl_fm -M fm -f $freq$M -l 0 -A std -p 0 -s 171k -g 26 -F 9 | /usr/local/bin/redsea -e | aplay -t raw -r 171000 -c 1 -f S16_LE
 }
 
-
-echo 'Redsea RDS decoder for FM Broadcasts
-  '
+echo '\nRedsea RDS decoder for FM Broadcasts'
 read -p 'Please enter a frequency in MHz: ' freq
 
 start
