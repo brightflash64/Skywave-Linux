f3cf00acedb62bed8d497ad5b5de43f9b196f188
brightflash64 <wm_munny92@yahoo.com>
2017-05-10 23:45:32 +0000

    Update rtlsdr-controller.sh

--- files/scripts/rtlsdr-controller.sh
+++ files/scripts/rtlsdr-controller.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
 
 #for rtl-sdr reception in ghpsdr3-alex / QtRadio
-no() {
-gnome-terminal -e 'rtlsdr-server -d3 -s1024000' &
-sleep 2
-gnome-terminal -e 'dspserver --lo 9000' &
-sleep 2
-QtRadio 127.0.0.1
-killall rtlsdr-server
-killall dspserver
-}
-
-yes() {
-gnome-terminal -e 'rtlsdr-server -d3 -s1024000' &
+runme() {
+gnome-terminal -e 'rtlsdr-server -d3 -g240 -s250000' &
 sleep 2
 gnome-terminal -e 'dspserver --lo 9000' &
 sleep 2
@@ -28,14 +18,13 @@ case "$choice1" in
   openssl genrsa -out pkey 2048
   openssl req -new -key pkey -out cert.req
   openssl x509 -req -days 365 -in cert.req -signkey pkey -out cert
-  yes
+runme
   echo ""
   echo ""
 ;;
   n|N )
-  no
+runme
   echo ""
   echo ""
 ;;
 esac
-
