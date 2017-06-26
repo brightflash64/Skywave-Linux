e43e008cd58f01cce79fe03c0f4ff3032a4dbc60
brightflash64 <wm_munny92@yahoo.com>
2017-06-26 05:46:55 +0000

    Update redsea-controller.sh
    Adjustments for higher gain of new librtlsdr.

--- files/scripts/redsea-controller.sh
+++ files/scripts/redsea-controller.sh
@@ -3,7 +3,7 @@ Encoding=UTF-8
 
 start() {
 M=M
-rtl_fm -M fm -f $freq$M -l 0 -A std -p 0 -s 171k -g 26 -F 9 | /usr/local/bin/redsea -e | aplay -t raw -r 171000 -c 1 -f S16_LE
+rtl_fm -M fm -f $freq$M -l 0 -A std -p 0 -s 171k -g 16 -F 9 | /usr/local/bin/redsea -e | aplay -t raw -r 171000 -c 1 -f S16_LE
 }
 
 echo '\nRedsea RDS decoder for FM Broadcasts'
