b7d5c6f6f4f2f7cc23ed693befbef9f8eb371314
brightflash64 <wm_munny92@yahoo.com>
2017-06-26 06:12:57 +0000

    Update sdr-updater.sh
    Edit version number.

--- sdr-updater.sh
+++ sdr-updater.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# sdr-updater for Skywave Linux, version 0.5
+# sdr-updater for Skywave Linux, version 0.6
 # Copyright (c) 2017 by Philip Collier, radio AB9IL <webmaster@ab9il.net>
 # SDR Updater is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
