7820c02d75744a52394935f4be0c613f1d6166e9
brightflash64 <wm_munny92@yahoo.com>
2017-06-26 05:37:53 +0000

    Update openwebrx-subcarrier.sh
    Adjustments for greater gain of new librtlsdr.

--- files/openwebrx/openwebrx-subcarrier.sh
+++ files/openwebrx/openwebrx-subcarrier.sh
@@ -11,23 +11,23 @@ sed -i "
      52s/.*/photo_title=\"Scenery from the Interneational Space Station\"/;
      73s/.*/samp_rate = 100000/;
      75s/.*/center_freq = 50000/;
-     76s/.*/rf_gain = 29/;
+     76s/.*/rf_gain = 14/;
      77s/.*/ppm = $corr/;
      79s/.*/audio_compression=\"none\"/;
-     94s/.*/start_rtl_command=\"rtl_fm -M fm -f $ctrfreq$mega -l 0 -A std -p $corr -s 400k -g 30 -F 9 - | csdr convert_s16_f | csdr shift_addition_cc -0.25 | csdr fir_decimate_cc 2 0.005\"/;
+     94s/.*/start_rtl_command=\"rtl_fm -M fm -f $ctrfreq$mega -l 0 -A fast -p $corr -s 400k -g 14 - | csdr convert_s16_f | csdr shift_addition_cc -0.25 | csdr fir_decimate_cc 2 0.002\"/;
      95s/.*/format_conversion=\"\"/;
      96s/.*/real_input = True/;
      137s/.*/client_audio_buffer_size = 10/;
      143s/.*/start_mod = \"$mode\"/;
-     150s/.*/waterfall_min_level = -80/;
-     151s/.*/waterfall_max_level = -25/" /usr/local/sbin/openwebrx/config_webrx.py
+     150s/.*/waterfall_min_level = -75/;
+     151s/.*/waterfall_max_level = -30/" /usr/local/sbin/openwebrx/config_webrx.py
 
 #Start OpenWebRX
 cd /usr/local/sbin/openwebrx/
 python ./openwebrx.py & firefox --new-tab http://localhost:8073/
 }
 
-OUTPUT=$(zenity --forms --title="OpenWebRX - RTL-SDR" --text="Enter the SDR start-up parameters." \
+OUTPUT=$(zenity --forms --title="OpenWebRX - FM Subcarriers" --text="Enter the SDR start-up parameters." \
 --separator="," --add-entry="Center Frequency (MHz)" --add-entry="Mode (am,fm,usb,lsb,cw)" --add-entry="Freq Correction (ppm)")
 
 if ((accepted != 0)); then
