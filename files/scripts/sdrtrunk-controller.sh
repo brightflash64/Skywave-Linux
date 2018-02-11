4b731d80b77cb1ea139a502eb7fee3294ab11b24
brightflash64 <wm_munny92@yahoo.com>
2018-02-11 23:32:16 +0000

    Update sdrtrunk-controller.sh

--- files/scripts/sdrtrunk-controller.sh
+++ files/scripts/sdrtrunk-controller.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-cd /opt/sdrtrunk
-./run_sdrtrunk_linux.sh
+# Copyright (c) 2018 by Philip Collier, radio AB9IL <webmaster@ab9il.net>
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version. There is NO warranty; not even for
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+cd /usr/local/sbin/sdrtrunk
+java -jar sdrtrunk.jar
