17c7663b8343cf892a578dc25243d2c5947a87c9
brightflash64 <wm_munny92@yahoo.com>
2018-02-11 22:52:13 +0000

    Update sdr-updater.sh
    Reconfigured for Skywave Linux 3.1.0

--- sdr-updater.sh
+++ sdr-updater.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# sdr-updater for Skywave Linux, version 0.6
+# sdr-updater for Skywave Linux, version 0.9
 # Copyright (c) 2017 by Philip Collier, radio AB9IL <webmaster@ab9il.net>
 # SDR Updater is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -9,155 +9,38 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 Encoding=UTF-8
 
-getqtradio(){
-#get qtradio
-echo "\n\n...QtRadio..."
-cd ~
-git clone https://github.com/alexlee188/ghpsdr3-alex
-cd ghpsdr3-alex
-sh cleanup.sh
-git checkout master
-autoreconf -i
-./configure
-make -j4 all
-make install
-cd ~
-rm -rf ghpsdr3-alex
-}
-
-getopenwebrx(){
-echo "\n\n...csdr dsp library..."
-# get the csdr dsp library
-cd ~
-git clone https://github.com/simonyiszk/csdr
-cd csdr
-make
-make install
-ldconfig
-cd ~
-rm -rf csdr
-echo "\n\n\n...Openwebrx..."
-# get openwebrx
-cd ~
-git clone https://github.com/simonyiszk/openwebrx
-# nothing to make
-# move the files
-cp -ar ~/openwebrx /usr/local/sbin/openwebrx
-
-# get the device support
-getairspy
-gethackrf
-getrtlsdr
-getsdrplay
-}
-
-getairspy(){
-echo "\n\n...airspy host..."
-#install airspy support
-cd ~
-git clone git://github.com/airspy/host/
-mkdir host/build
-cd host/build
-cmake ../ -DINSTALL_UDEV_RULES=ON
-make
-make install
-ldconfig
-
-#get the SoapyAirspy support module
-echo "\n\n...soapy airspy"
-cd ~
-git clone https://github.com/pothosware/SoapyAirspy
-mkdir SoapyAirspy/build
-cd SoapyAirspy/build
-cmake ..
-make
-make install
-ldconfig
-cd ~
-rm -rf SoapyAirspy
-rm -rf host
-}
-
-gethackrf(){
-#install hackrf support
-echo "\n\n...Mossmann's driver for hackrf"
-cd ~
-git clone https://github.com/mossmann/hackrf
-mkdir hackrf/host/build
-cd hackrf/host/build
-cmake ../ -DINSTALL_UDEV_RULES=ON
-make
-make install
-ldconfig
-
-#get the SoapyHackRF support module
-echo "\n\n...soapy hackrf"
-cd ~
-git clone https://github.com/pothosware/SoapyHackRF
-mkdir SoapyHackRF/build
-cd SoapyHackRF/build
-cmake ..
-make
-make install
-ldconfig
-cd ~
-rm -rf hackrf
-rm -rf SoapyHackRF
+aptupgrade(){
+apt update
+apt -y upgrade
+echo "\n\nFinished installing software from the repositories."
 }
 
 getsdrplay(){
-#open source sdrplay driver from f4exb
-cd ~
-git clone https://github.com/f4exb/libmirisdr-4
-mkdir libmirisdr-4/build
-cd libmirisdr-4/build
-cmake ../
-make
-make install
-ldconfig
-cd ~
-rm -rf libmirisdr-4
-
 #get the sdrplay linux api installer manually
 #from http://sdrplay.com/linuxdl.php
 #then enable and run it:
-echo "\n\n...SDRplay MiricsAPI..."
-echo "\nGet it manually from http://sdrplay.com/"
-echo "\nRobots are people too."
-cd ~
-chmod 755 SDRplay_RSP_API-Linux-2.09.1.run
-./SDRplay_RSP_API-Linux-2.09.1.run
-rm -f SDRplay_RSP_API-Linux-2.09.1.run
+#echo "\n\n...SDRplay MiricsAPI..."
+#echo "\nGet it manually from http://sdrplay.com/"
+#echo "\nRobots are people too."
+#cd ~
+#chmod 755 SDRplay_RSP_API-Linux-2.10.2.run
+#./SDRplay_RSP_API-Linux-2.10.2.run
+#rm -f SDRplay_RSP_API-Linux-2.10.2.run
 
-#get the SoapySDRPlay support module for CubicSDR
-echo "\n...SoapySDRPlay..."
-cd ~
-git clone https://github.com/pothosware/SoapySDRPlay
-mkdir SoapySDRPlay/build
-cd SoapySDRPlay/build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
-make install
-ldconfig
-cd ~
-rm -rf SoapySDRPlay
+#open source sdrplay driver from f4exb
+#cd ~
+#git clone https://github.com/f4exb/libmirisdr-4
+#mkdir libmirisdr-4/build
+#cd libmirisdr-4/build
+#cmake ../
+#make
+#make install
+#ldconfig
+#cd ~
+#rm -rf libmirisdr-4
 
-#get sdrplay support from osmocom
-echo "\n\n...gr-osmosdr (gnuradio dependencies for sdrplay forked by hb9fxq)"
-cd ~
-#use the hb9fxq fork with better sdrplay support
-git clone https://github.com/krippendorf/gr-osmosdr-fork-sdrplay
-mkdir gr-osmosdr-fork-sdrplay/build
-cd gr-osmosdr-fork-sdrplay/build
-cmake -DENABLE_NONFREE=TRUE ../
-make
-make install
-ldconfig
-cd ~
-rm -rf gr-osmosdr-fork-sdrplay
-
-echo "\n\n...openwebrx dependencies for sdrplay (SDRPlayPorts)"
 #SDRplay support in OpenWebRX
+echo "\n\n...openwebrx dependencies for sdrplay (SDRPlayPorts)"
 cd ~
 git clone https://github.com/krippendorf/SDRPlayPorts
 mkdir SDRPlayPorts/build
@@ -175,69 +58,20 @@ getrtlsdr(){
 echo "\n\n...rtl-sdr firmware..."
 cd ~
 #git clone https://git.osmocom.org/rtl-sdr
+#git clone https://github.com/steve-m/librtlsdr
+#git clone https://github.com/mutability/rtl-sdr
 #git clone https://github.com/thaolia/librtlsdr-thaolia
 #mv librtlsdr-thaolia rtl-sdr
-git clone https://github.com/mutability/rtl-sdr
+git clone https://github.com/AB9IL/rtl-sdr
 mkdir rtl-sdr/build
 cd rtl-sdr/build
 cmake ../ -DINSTALL_UDEV_RULES=ON
 make
 make install
 ldconfig
-
-#get the SoapyRTLSDR support module
-echo "\n\n...SoapyRTLSDR..."
-cd ~
-git clone https://github.com/pothosware/SoapyRTLSDR
-mkdir SoapyRTLSDR/build
-cd SoapyRTLSDR/build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
-make install
-ldconfig
-
-#install rtl_hpsdr
-#build librtlsdr, but only keep rtl_hpsdr
-echo "\n\n...rtl_hpsdr..."
-cd ~
-git clone https://github.com/n1gp/librtlsdr
-mkdir librtlsdr/build
-cd librtlsdr/build
-cmake ..
-make
-cp ~/librtlsdr/build/src/rtl_hpsdr /usr/local/bin/rtl_hpsdr
-cd ~
-rm -rf rtl-sdr
-rm -rf librtlsdr
-rm -rf SoapyRTLSDR
 }
 
-getsoapysdr(){
-#get SoapySDR
-echo "\n\n...SoapySDR..."
-cd ~
-git clone https://github.com/pothosware/SoapySDR
-mkdir SoapySDR/build
-cd SoapySDR/build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
-make install
-ldconfig
-
-#get rtaudio
-echo "\n\n...rtaudio"
-cd ~
-git clone https://github.com/thestk/rtaudio
-#mkdir rtaudio/build
-#cd rtaudio/build
-#cmake .. -DAUDIO_LINUX_PULSE=ON
-cd rtaudio ./autogen.sh --with-pulse
-make
-make install
-ldconfig
-cd ~
-rm -rf rtaudio
-
+getsoapy(){
 #get the SoapyAudio support module
 echo "\n\n...soapy audio"
 cd ~
@@ -250,72 +84,131 @@ make install
 ldconfig
 cd ~
 rm -rf SoapyAudio
-rm -rf SoapySDR
 
-#get the SoapyOsmo support module
-echo "\n\n...soapy osmo"
+#get SoapyPlutoSDR support module
+echo "\n\n...SoapyPlutoSDR"
 cd ~
-git clone https://github.com/pothosware/SoapyOsmo
-mkdir SoapyOsmo/build
-cd SoapyOsmo/build
+git clone https://github.com/jocover/SoapyPlutoSDR
+mkdir SoapyPlutoSDR/build
+cd SoapyPlutoSDR/build
 cmake ..
+make
+sudo make install
+ldconfig
+
+#get the SoapySDRPlay support module for CubicSDR
+echo "\n\n...SoapySDRPlay..."
+cd ~
+git clone https://github.com/pothosware/SoapySDRPlay
+mkdir SoapySDRPlay/build
+cd SoapySDRPlay/build
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make
 make install
 ldconfig
 cd ~
-rm -rf SoapyAudio
-rm -rf SoapyOsmo
+rm -rf SoapySDRPlay
+}
+
+getqtradio(){
+#get qtradio
+echo "\n\n...QtRadio..."
+cd ~
+git clone https://github.com/alexlee188/ghpsdr3-alex
+cd ghpsdr3-alex
+sh cleanup.sh
+git checkout master
+autoreconf -i
+./configure
+make -j4 all
+make install
+cd ~
+rm -rf ghpsdr3-alex
+
+#Create a better launcher
+echo '[Desktop Entry]
+Type=Application
+Name=QtRadio
+Name[xx]=QtRadio
+Generic Name=SDR GUI
+Exec=QtRadio
+Icon=QtRadio
+Terminal=False
+Categories=;HamRadio;' > /usr/local/share/applications/QtRadio.desktop
+}
+
+getopenwebrx(){
+echo "\n\n...csdr dsp library..."
+# get the csdr dsp library
+cd ~
+git clone https://github.com/simonyiszk/csdr
+cd csdr
+make
+make install
+ldconfig
+cd ~
+rm -rf csdr
+echo "\n\n...Openwebrx..."
+# get openwebrx
+cd ~
+git clone https://github.com/simonyiszk/openwebrx
+# nothing to make
+# move the files
+cp -ar ~/openwebrx /usr/local/sbin/openwebrx
 }
 
 getcubicsdr(){
 #install CubicSDR and dependencies
-echo "\n\n...Getting CubicSDR and dependencies"
-echo "\n...liquid-dsp..."
 cd ~
-git clone https://github.com/jgaeddert/liquid-dsp
-cd liquid-dsp
-./bootstrap.sh
-./configure
-make
-make install
-ldconfig
-
-#get SoapySDR
-getsoapysdr
-
-#get basic radio firmware and soapy modules
-getrtlsdr
-getairspy
-gethackrf
-
-#get CubicSDR
-echo "\n\n...CubicSDR..."
-cd ~
-git clone https://github.com/cjcliffe/CubicSDR
-mkdir CubicSDR/build
-cd CubicSDR/build
-cmake ../
-make
-
-#move it to /opt
-#mkdir /opt/CubicSDR
-cp -ar ~/CubicSDR/build/x64/* /opt/CubicSDR
-cd ~
-rm -rf liquid-dsp
-rm -rf CubicSDR
+wget "https://github.com/cjcliffe/CubicSDR/releases/download/0.2.3/CubicSDR-0.2.3-x86_64.AppImage"
+chmod +x CubicSDR-0.2.3-x86_64.AppImage
+mv ~/CubicSDR-0.2.3-x86_64.AppImage /usr/local/sbin/CubicSDR/CubicSDR.AppImage
 }
 
-getcudasdr(){
-echo "\n\n...cudaSDR..."
+getremotesdrclient(){
+#get RemoteSdrClient-NS (for RFSpace hardware)
 cd ~
-git clone https://github.com/n1gp/cudaSDR
-cd cudaSDR/Source
-qmake cudaSDR.pro
+git clone https://github.com/csete/remotesdrclient-ns
+cd remotesdrclient-ns
 make
-cp ~/cudaSDR/Source/bin/cudaSDR /usr/local/bin/cudaSDR
-cp ~/cudaSDR/Source/bin/settings.ini /usr/local/bin/settings.ini
+#manually copy the binary to /usr/local/bin
+cp ~/remotesdrclient-ns/remotesdrclient-ns /usr/local/bin/remotesdrclient-ns
+#manually copy the icon to /usr/share/pixmaps
+cp ~/remotesdrclient-ns/RemoteSdrClient.png /usr/share/pixmaps/RemoteSdrClient.png
+#create menu entry via .desktop file
+echo '[Desktop Entry]
+Name=RemoteSdrClient
+GenericName=RemoteSdrClient
+Comment=Remote Client for RFSpace SDRs
+Exec=/usr/local/bin/remotesdrclient-ns
+Icon=RemoteSdrClient.png
+Terminal=false
+Type=Application
+Categories=Network;HamRadio;' > /usr/share/applications/remotesdrclient.desktop
+}
+
+rtlsdrairband(){
+#get rtlsdr-airband
+git clone https://github.com/szpajder/RTLSDR-Airband
+cd RTLSDR-Airband
+PLATFORM=x86 NFM=1 make
+make install
+
+#get acarsdec
 cd ~
-rm -rf cudaSDR
+git clone https://github.com/AB9IL/acarsdec
+cd acarsdec
+autoreconf
+./configure
+make
+make acarsserv
+
+cd ~
+#get dumpvdl2
+git clone https://github.com/szpajder/dumpvdl2
+cd dumpvdl2
+make
+cp ~/dumpvdl2/dumpvdl2 /usr/local/bin/dumpvdl2
 }
 
 getdump1090(){
@@ -334,22 +227,6 @@ cp README.md /usr/local/sbin/dump1090/README.md
 cd ~
 rm -rf dump1090
 echo "\n\n...completed update for dump1090 for rtl-sdr devices..."
-
-#get dump1090 with advanced device support
-echo "\n\n...dump1090 for advanced devices..."
-cd ~
-git clone https://github.com/itemir/dump1090_sdrplus
-cd dump1090_sdrplus
-make
-cp -ar images /usr/local/sbin/dump1090_sdrplus/images
-cp -ar testfiles /usr/local/sbin/dump1090_sdrplus/testfiles
-cp -ar tools /usr/local/sbin/dump1090_sdrplus/tools
-cp gmap.html /usr/local/sbin/dump1090_sdrplus/gmap.html
-cp dump1090 /usr/local/sbin/dump1090_sdrplus/dump1090
-cp README.md /usr/local/sbin/dump1090_sdrplus/README.md
-cd ~
-rm -rf dump1090_sdrplus
-echo "\n\n...completed update for dump1090 for advanced devices..."
 }
 
 getr820tweak(){
@@ -364,6 +241,39 @@ rm -rf r820tweak
 ldconfig
 }
 
+getsdrtrunk(){
+#sdrtrunk
+echo "\n\n...SDRTrunk..."
+cd ~
+wget "https://github.com/DSheirer/sdrtrunk/releases/download/v0.3.3-beta.3/sdr-trunk-all-0.3.3-beta.3.jar"
+mv sdr-trunk-all-0.3.3-beta.3.jar /usr/local/sbin/sdrtrunk.jar
+}
+
+getwsjtx(){
+#get WSJT-X
+echo "\n\n...WSJT-X..."
+cd ~
+wget "http://physics.princeton.edu/pulsar/k1jt/wsjtx_1.8.0_amd64.deb"
+dpkg -i wsjtx_1.8.0_amd64.deb
+}
+
+getrtaudio(){
+#get rtaudio
+echo "\n\n...rtaudio"
+cd ~
+git clone https://github.com/thestk/rtaudio
+mkdir rtaudio/build
+cd rtaudio/build
+cmake .. -DAUDIO_LINUX_PULSE=ON
+cd rtaudio
+./autogen.sh --with-pulse
+make
+make install
+ldconfig
+cd ~
+rm -rf rtaudio
+}
+
 getcrypto(){
 #lantern
 echo "\n\n...getting Lantern..."
@@ -376,23 +286,42 @@ Type=Application
 Name=Lantern
 Exec=sh -c "lantern -addr 127.0.0.1:8118"
 Icon=lantern
-Comment=Censorship circumvention application for unblocked web browsing.
+Comment=Censorship circumvention (proxy / vpn) application for unblocked web browsing.
 Categories=Network;Internet;Networking;Privacy;Proxy;VPN;' > /usr/share/applications/lantern.desktop
 
 echo "\n\n...cleaning up a bit..."
 
 #update cjdns
-echo "\n\...updating cjdns..."
+echo "\n\n...updating cjdns..."
 sh -c "/etc/init.d/cjdns update"
+
+# get psiphon
+# optionally, use this dev's repo: https://github.com/thispc/psiphon
+git clone https://github.com/gilcu3/psiphon
+cd psiphon
+
+#get openssh-portable for psiphon
+#optionally, use version 5.9p1
+cd ~
+git clone https://github.com/openssh/openssh-portable
+cd openssh-portable
+autoreconf
+./configure
+make
+# move up one level then copy the ssh binary to the Psiphon directory.
+cd ..
+rm ssh
+cp openssh-portable/ssh .
 }
 
 ans=$(zenity  --list --height 270 --width 420 --text "SDR Software Updater" --radiolist  --column "Pick" --column "Action" \
-TRUE "Exit (Do Nothing)" FALSE "Update QtRadio" FALSE "Update CubicSDR" FALSE "Update CudaSDR"  FALSE "Update OpenwebRX" \
-FALSE "Update R820tweak" FALSE "Update Dump1090" FALSE "Update HackRF Drivers" FALSE "Update SDRPlay Drivers" \
-FALSE "Update RTL-SDR Drivers" FALSE "Update Mesh Networking & Crypto");
+TRUE "Upgrade Base System and Drivers with Apt" FALSE "Update QtRadio" FALSE "Update CubicSDR" FALSE "Update RemoteSdrClient" \
+FALSE "Update OpenwebRX" FALSE "Update R820tweak" FALSE "Update SoapySDR Drivers" FALSE "Update Dump1090" \
+FALSE "Update SDRPlay Drivers" FALSE "Update RTL-SDR"  FALSE "Update RTLSDR-Airband" FALSE "Update SDRTrunk" \
+FALSE "Update WSJT-X" FALSE "Update Rtaudio" FALSE "Update Mesh Networking & Crypto");
 
-	if [  "$ans" = "Exit (Do Nothing)" ]; then
-		exit
+	if [  "$ans" = "Upgrade Base System and Drivers with Apt" ]; then
+		aptupgrade
 
 	elif [  "$ans" = "Update QtRadio" ]; then
 		getqtradio
@@ -400,8 +329,8 @@ FALSE "Update RTL-SDR Drivers" FALSE "Update Mesh Networking & Crypto");
 	elif [  "$ans" = "Update CubicSDR" ]; then
 		getcubicsdr
 
-	elif [  "$ans" = "Update CudaSDR" ]; then
-		getcudasdr
+	elif [  "$ans" = "Update RemoteSdrClient" ]; then
+		getremotesdrclient
 
 	elif [  "$ans" = "Update Dump1090" ]; then
 		getdump1090
@@ -412,14 +341,26 @@ FALSE "Update RTL-SDR Drivers" FALSE "Update Mesh Networking & Crypto");
 	elif [  "$ans" = "Update R820tweak" ]; then
 		getr820tweak
 
-	elif [  "$ans" = "Update HackRF Drivers" ]; then
-		gethackrf
+	elif [  "$ans" = "Update SoapySDR Drivers" ]; then
+		getsoapy
 
 	elif [  "$ans" = "Update SDRPlay Drivers" ]; then
 		getsdrplay
 
 	elif [  "$ans" = "Update RTL-SDR Drivers" ]; then
 		getrtlsdr
+
+	elif [  "$ans" = "Update RTLSDR-Airband" ]; then
+		rtlsdrairband
+
+	elif [  "$ans" = "Update SDRTrunk" ]; then
+		getsdrtrunk
+
+	elif [  "$ans" = "Update WSJT-X" ]; then
+		getwsjtx
+
+	elif [  "$ans" = "Update Rtaudio" ]; then
+		getrtaudio
 
 	elif [  "$ans" = "Update Mesh Networking & Crypto" ]; then
 		getcrypto
