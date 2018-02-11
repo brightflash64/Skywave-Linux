3315f4cd24809e5d29959a9cd1b3140f6b0d610d
brightflash64 <wm_munny92@yahoo.com>
2018-02-11 23:28:19 +0000

    Update r820tweak.sh

--- files/scripts/r820tweak.sh
+++ files/scripts/r820tweak.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+# Copyright (c) 2018 by Philip Collier, radio AB9IL <webmaster@ab9il.net>
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version. There is NO warranty; not even for
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
 
 r820tweak gqrx &
 sleep 5
